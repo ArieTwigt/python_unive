44a8242720b80436c669ce505f233d5b062e6d4f
Arie Twigt <arietwigt@MacBook-Air-van-Arie.local>
2022-03-24 16:26:13 +0100

    added assignments for 2

---
+++ 2_assignments.py
@@ -0,0 +1,13 @@
+# 1. Maak een variable aan die het aantal dagen tot en met
+# kerst weergeeft
+
+
+# 2. Bedenk de code die de oppervlakte van een 
+# cirkel berekend (straal kwadraat * pi)
+
+
+# 3. Creeer een lijst die de bestanden in je huidige 
+# projectmap bevat
+
+
+# 4. Maak vanuit Python (os) een extra map aan 'our_functions'
--- 2_d_os.py
+++ 2_d_os.py
@@ -1,14 +1,16 @@
-#%% 
+#%% importeer de os library
 import os
-# %%
+
+# %% maak een map aan 
 os.mkdir("my_folder")
-# %%
+
+# %% verwijder een file
 os.remove("my_file.txt")
-# %%
+
+# %% geef de bestanden aan in de huidige working directory
 os.listdir()
-# %%
+# %% geef de huidige working directory aan
 os.getcwd()
-# %%
+
+# %% geef de bestanden weer in de betreffende folder
 current_files = os.listdir('my_folder')
-# %%
-# %%
